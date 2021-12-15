1e19b3223718be90cc9b2fc8d34bef84d03bcb1f
CatchZ <83031014+CatchZ@users.noreply.github.com>
2021-12-15 08:39:59 +0100

    Update index.js
    add Vorlesungshinhalt express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const port = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (req, res) => {
+ res.send({headers: req.headers})
+})
+app.listen(port, () => {
+ console.log(`Example app listening at http://localhost:${port}`)
+})
