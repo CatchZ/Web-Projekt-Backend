511e7c945d2f87c565e946e898d910062450057f
CatchZ <maximilian.ziemann@gmx.de>
2021-12-15 19:19:14 +0100

    schön gemacht

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
- res.send({headers: req.headers})
+  res.send({headers: req.headers})
 })
 app.listen(port, () => {
- console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
