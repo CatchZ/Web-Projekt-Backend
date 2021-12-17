08509c4eb599316437a9f478d54571c313906569
CatchZ <maximilian.ziemann@gmx.de>
2021-12-17 08:12:15 +0100

    bla

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
+import { Request,Response } from "express";
 const express = require('express')
 const app = express()
 const port = const port = process.env.PORT || 3000
-app.get('/', (req, res) => {
-  res.send({headers: req.headers})
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+res.send('Hello World!')
 })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
