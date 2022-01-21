e7211ada76f6a08f013a49f5924b9437b74e5b21
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-01-21 21:14:28 +0100

    typo

--- build/app.js
+++ build/app.js
@@ -128,8 +128,8 @@ app.post("/sendRegistrationMail", async (req, res) => {
     });
     const options = {
         from: "wad2122@outlook.de",
-        to: mailData.username,
-        subject: "Empfaengertest",
+        to: mailData.email, //irgendwas hier wahrscheinlich falsch??
+        subject: "Empfaengertest2",
         text: "Klappt "
     };
     transporter.sendMail(options, function (err, info) {
